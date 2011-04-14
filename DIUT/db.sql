bac1fcc1705c13c2335e52049a7fdce6dfd13850
ApHu <aphu@aphu-desktop.(none)>
2011-04-14 02:29:01 -0430

    cambios de la templates

---
+++ DIUT/db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE personas (
+        id          INTEGER PRIMARY KEY,
+        nombre      VARCHAR ,
+        apellido    VARCHAR ,
+        cedula      VARCHAR ,
+        telefono    VARCHAR ,
+        correo      VARCHAR
+);
+
